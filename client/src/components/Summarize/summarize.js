a8a2d546a17b32733f469f21ea5d32afdbe2f68c
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-02-25 17:01:32 +0530

    added proper apis
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

--- client/src/components/Summarize/summarize.js
+++ client/src/components/Summarize/summarize.js
@@ -23,7 +23,7 @@ const Summarize = () => {
     if (url) {
       setLoading(true);
       axios
-        .post(`${apiUrl}/transcript`, { url, lang: language })
+        .post(`${apiUrl}`, { url, lang: language })
         .then((response) => {
           setSummary(response.data.data);
           setLoading(false);
